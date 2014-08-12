5b1774dc17b46902d94b3d039dc73630a1d7c0c1
Alex Logashov <admin@shiftarray.com>
2014-08-12 14:29:00 -0700

    Init

---
+++ README.md
@@ -0,0 +1,49 @@
+# grunt-uglify-amd
+
+> Use "@inlude:" in file comments to sepcify module dependencies instead of manually creating an array of file dependencies.
+
+## Getting Started
+This plugin requires Grunt `~0.4.5`
+
+If you haven't used [Grunt](http://gruntjs.com/) before, be sure to check out the [Getting Started](http://gruntjs.com/getting-started) guide, as it explains how to create a [Gruntfile](http://gruntjs.com/sample-gruntfile) as well as install and use Grunt plugins. Once you're familiar with that process, you may install this plugin with this command:
+
+```shell
+npm install grunt-uglify-amd --save-dev
+```
+
+Once the plugin has been installed, it may be enabled inside your Gruntfile with this line of JavaScript:
+
+```js
+grunt.loadNpmTasks('grunt-uglify-amd');
+```
+
+## The "uglifyAMD" task
+
+### Overview
+In your project's Gruntfile, add a section named `uglifyAMD` to the data object passed into `grunt.initConfig()`.
+
+```js
+grunt.initConfig({
+  uglifyAMD: {
+    options: {
+      // Task-specific options go here.
+    },
+    files: {
+      // Target-specific file lists and/or options go here.
+    },
+  },
+});
+```
+
+### Specify Dependencies
+
+```js
+// @include: scripts/src/shift/utils.js 
+// @include: scripts/src/shift/tweet.js
+```
+
+## Contributing
+In lieu of a formal styleguide, take care to maintain the existing coding style. Add unit tests for any new or changed functionality. Lint and test your code using [Grunt](http://gruntjs.com/).
+
+## Release History
+_(Nothing yet)_
---
+++ tasks/uglifyAMD.js
@@ -0,0 +1,86 @@
+/*
+ * grunt-uglify-amd
+ * https://github.com/logashoff/grunt-uglify-amd
+ *
+ * Copyright (c) 2014 Alex Logashov
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+var _ = require("lodash");
+
+module.exports = function(grunt) 
+{
+  // Please see the Grunt documentation for more information regarding task
+  // creation: http://gruntjs.com/creating-tasks
+  
+  function filePathsFromString(value)
+  {
+    var strArr = value.match(/\s+@include:\s+(\S+)/gmi);
+
+    if(!strArr || !strArr.length) return [];
+
+    for(var i = strArr.length, s, r = []; i-- > 0;)
+    {
+        s = strArr[i];
+        
+        if(s && !~r.indexOf(s))
+        {
+          r.push(s.match(/(\S+)$/)[0]);
+        }
+    }
+
+    return r;
+  }
+
+  function buildPaths(file, arr)
+  {
+    if(!grunt.file.exists(file))
+    {
+      grunt.log.warn('File "' + file + '" not found');
+      return;
+    }
+
+    arr.unshift(file);
+
+    var body = grunt.file.read(file);
+    var amds = filePathsFromString(body);
+
+    if(amds && amds.length)
+    {
+      for(var i = 0, l = amds.length; i < l; i++)
+      {
+        buildPaths(amds[i], arr);
+      }
+    }
+
+    return arr;
+  }
+
+  grunt.registerMultiTask('uglifyAMD', 'Create array of module dependencies from @inlude comments', function() 
+  {
+    if(!this.files || !this.files.length)
+    {
+      grunt.log.error("File target not specified")
+      return;
+    }
+
+    grunt.task.exists("uglify");
+
+    var options   = this.options(),
+        firstFile = this.files[0],
+        filePaths = [];
+
+    buildPaths(firstFile.src[0], filePaths);
+
+    var files = {};
+        files[firstFile.dest] = _.uniq(filePaths);
+
+    grunt.config.set("uglify.js.options", options);
+    grunt.config.set("uglify.js.files", files);
+    grunt.task.run("uglify");
+
+  });
+
+};
